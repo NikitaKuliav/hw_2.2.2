016e2783cbd17cb696d28538a7e4605738338e44
Nikita <athlon64x2600@gmail.com>
2022-09-20 21:21:14 +0200

    hw 2.2.2

--- src/Truck.java
+++ src/Truck.java
@@ -1,17 +1,29 @@
-public class Truck {
+public class Truck extends WheelTransport implements Serviceable, Engineable {
+    public Truck(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+    @Override
+    public void check() {
 
-    public String modelName;
-    public int wheelsCount;
+        System.out.println("Обслуживаем " + getModelName() + " грузовик");
+        for (int i = 0; i < getWheelsCount(); i++) {
+            updateTyre();
+        }
+        checkEngine();
+        checkTrailer();
 
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
+    }
+    @Override
+     public void updateTyre() {
+        System.out.println("Меняем покрышку у грузовика");
     }
 
-    public void checkEngine() {
-        System.out.println("Проверяем двигатель");
+     @Override
+     public void checkEngine() {
+        System.out.println("Проверяем двигатель у грузовика");
     }
 
-    public void checkTrailer() {
-        System.out.println("Проверяем прицеп");
+     private void checkTrailer() {
+        System.out.println("Проверяем прицеп у грузовика");
     }
 }
